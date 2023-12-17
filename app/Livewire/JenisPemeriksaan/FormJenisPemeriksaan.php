023708f7059fb1e4aadaad8b7a31d18f2454aea6
Ariston-mac <aristonbobi@gmail.com>
2023-12-17 20:38:40 +0800

    update

--- app/Livewire/JenisPemeriksaan/FormJenisPemeriksaan.php
+++ app/Livewire/JenisPemeriksaan/FormJenisPemeriksaan.php
@@ -3,11 +3,16 @@
 namespace App\Livewire\JenisPemeriksaan;
 
 use Livewire\Component;
+use App\Models\jenis_pengujian_sampel;
+use App\Models\analisaSampel;
 
 class FormJenisPemeriksaan extends Component
-{
+{   public $jenisPengujian ='';
+    public $analisa="";
     public function render()
     {
-        return view('livewire.jenis-pemeriksaan.form-jenis-pemeriksaan');
+        $data=jenis_pengujian_sampel::all();
+        $analisa = analisaSampel::where('jenisPengujian_id',$this->jenisPengujian)->get();
+        return view('livewire.jenis-pemeriksaan.form-jenis-pemeriksaan',['query'=>$data,'analisaSampel'=>$analisa]);
     }
 }
--- resources/views/livewire/jenis-pemeriksaan/form-jenis-pemeriksaan.blade.php
+++ resources/views/livewire/jenis-pemeriksaan/form-jenis-pemeriksaan.blade.php
@@ -4,11 +4,16 @@
         </div>
 
         <div class="card-body row">
-           
+
             <div class="form-group col-lg-12 col-md-12 col-sm-12 row" >
                 <label class="control-label col-sm-4">Jenis Analisa <b class='text-red'>*</b></label>
                 <div class="col-sm-8">
-                    <input type="text" wire:model.defer='varNomor_Surat' class="form-control rounded-0  @error('varNomor_Surat') is-invalid @enderror" placeholder="Jenis Pengujian Sampel">
+                    <select class="form-control" wire:model.live='jenisPengujian'>
+                        <option value="">--Pilih Salah Satu--</option>
+                        @foreach ( $query as $data)
+                            <option value="{{ $data->id }}" >{{ $data->jenis_pengujian }}</option>
+                        @endforeach
+                    </select>
                      @error('varFile') <span class=" text-red">{{ $message }}</span> @enderror
                 </div>
             </div>
@@ -16,7 +21,12 @@
             <div class="form-group col-lg-12 col-md-12 col-sm-12 row" >
                 <label class="control-label col-sm-4">Ketegori Pengujian <b class='text-red'>*</b></label>
                 <div class="col-sm-8">
-                    <input type="text" wire:model.defer='varNomor_Surat' class="form-control rounded-0  @error('varNomor_Surat') is-invalid @enderror" placeholder="Jenis Pengujian Sampel">
+                    <select class="form-control" wire:model='analisa'>
+                        <option value="">--Pilih Salah Satu---</option>
+                        @foreach ( $analisaSampel as $data)
+                        <option value="{{ $data->id }}" >{{ $data->jenis_analisa }}</option>
+                        @endforeach
+                    </select>
                      @error('varFile') <span class=" text-red">{{ $message }}</span> @enderror
                 </div>
             </div>
@@ -53,5 +63,5 @@
         <div class="card-footer">
             <button class="btn btn-sm btn-primary float-right">Simpan</button>
         </div>
-        
+
     </div>
