9f60c4528bd9156837123d4b03326a32152db9ae
ariston <aristonbobi@gmail.com>
2023-12-27 06:47:31 +0800

    update

--- resources/views/livewire/permohonan-analisis/form-permintaan-analisis.blade.php
+++ resources/views/livewire/permohonan-analisis/form-permintaan-analisis.blade.php
@@ -1,49 +1,37 @@
-<div class="col-lg-12 row">
-    <form wire:submit='store' class="row form-horizontal">
-        <div class="col-lg-12 col-md-12 col-sm-12 row">
+<div class="col-lg-12 ">
+    <form wire:submit='store' class=" form-horizontal">
+        <div class="col-lg-12 col-md-12 col-sm-12 ">
             <div class="card card-outline card-primary">
                 <div class="card-header ">
-                    <h5 class="card-title">FORM <b>IDENTITAS PERMOHONAN ANALISIS</b></h5>
+                    <h5 class="card-title"><i class="fa fa-address-card" aria-hidden="true"></i> FORM <b>IDENTITAS PERMOHONAN ANALISIS</b></h5>
                     <a class="btn btn-warning btn-sm float-right"><i class="fa fa-plus"></i> <b>Data Pemohon</b></a>
                 </div>
                 <div class="card-body row">
                     <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label col-sm-3"> Nomor SPK <b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                           <input type="text" wire:model='nomorSpk' class="disabled form-control @error('nomorSpk') is-invalid @enderror" placeholder="Nomor SPK">
+                           <input type="text" wire:model='nomorSpk' disabled class=" form-control @error('nomorSpk') is-invalid @enderror" placeholder="Nomor SPK">
                             @error('nomorSpk') <span class="error is-invalid text-red"> {{ $message }} </span> @enderror
-                        </div>
-                    </div>
-                    <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
-                        <label class="control-label col-sm-3"> Jenis Pengujian Sampel <b class='text-red'>*</b></label>
-                        <div class="col-sm-7">
-                            <select wire:model='jenisPengujian' class=" form-control @error('jenisPengujian') is-invalid @enderror">
-                                <option value="">-- Pilih Salah Satu --</option>
-                                @foreach ($analisa as $data )
-                                <option value="{{ $data->id }}">{{ $data->jenis_pengujian}}</option>
-                                @endforeach
-                            <select>
-                            @error('jenisPengujian') <span class=" error is-invalid text-red"> {{ $message }} </span> @enderror
                         </div>
                     </div>
                     <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
                         <label class=" control-label col-sm-3">Nama Pemohon <b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                            <input type="text" wire:model.defer='namaPemohon' class="form-control rounded-0  @error('namaPemohon') is-invalid @enderror" placeholder=" Nama Pemohon">
+                            <input type="text" wire:model.defer='namaPemohon' class="form-control rounded-0  @error('namaPemohon') is-invalid @enderror" placeholder=" Nama Pemohon" disabled>
                             @error('namaPemohon') <span class=" text-red error">{{ $message }}</span> @enderror
                         </div>
                     </div>
                     <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label col-sm-3">No. Telp (HP) <b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                            <input type="text" wire:model='noTlpn' class="form-control rounded-0  @error('noTlpn') is-invalid @enderror" placeholder=" Nomor Telepon (HP)">
+                            <input type="text" wire:model='noTlpn' class="form-control rounded-0  @error('noTlpn') is-invalid @enderror" placeholder=" Nomor Telepon (HP)" disabled>
                             @error('noTlpn')<span class="error is_invalid  text-red"> {{ $message }} </span>@enderror
                         </div>
                     </div>
                     <div class="form-group  col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label  col-sm-3">Provinsi<b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                            <select class=" form-control @error('provinsi') is-invalid @enderror" wire:model='provinsi' >
+                            <select class=" form-control @error('provinsi') is-invalid @enderror" wire:model='provinsi' disabled>
                                 <option value="">-- Pilih Salah Satu ---</option>
                             <select>
                             @error('provinsi')<span class="error text-red ">{{ $message }}</span>@enderror
@@ -52,7 +40,7 @@
                     <div class="form-group  col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label col-sm-3">Kab/Kota<b class='text-red'>*</b></label>
                         <div class="col-sm-7" >
-                            <select wire:model='kab' class="form-control  @error('kab') is-invalid @enderror" >
+                            <select wire:model='kab' class="form-control  @error('kab') is-invalid @enderror" disabled>
                                 <option value="">-- Pilih Salah Satu ---</option>
                             <select>
                             @error('kab')<span class="error text-red ">{{ $message }}</span>@enderror
@@ -61,7 +49,7 @@
                     <div class="form-group  col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label  col-sm-3">Kecamatan<b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                            <select class=" @error('kec') is-invalid @enderror form-control" >
+                            <select class=" @error('kec') is-invalid @enderror form-control" disabled>
                                 <option value="">-- Pilih Salah Satu ---</option>
                             <select>
                             @error('kec')<span class="error text-red ">{{ $message }}</span>@enderror
@@ -70,7 +58,7 @@
                     <div class="form-group  col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label col-sm-3">Kelurahan/Desa<b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                            <select class="form-control @error('kel') is-invalid @enderror" >
+                            <select class="form-control @error('kel') is-invalid @enderror" disabled>
                                 <option value="">-- Pilih Salah Satu ---</option>
                             <select>
                             @error('kel')<span class="error text-red ">{{ $message }}</span>@enderror
@@ -79,19 +67,31 @@
                     <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
                         <label class="control-label  col-sm-3">Tanggal Pengujian<b class='text-red'>*</b></label>
                         <div class="col-sm-7">
-                            <input type="text" wire:model.live='tanggal' class="form-control rounded-0  @error('tanggal') is-invalid @enderror" placeholder="Tanggal">
+                            <input type="text" wire:model.live='tanggal' class="form-control rounded-0  @error('tanggal') is-invalid @enderror" placeholder="Tanggal" disabled>
                             @error('tanggal') <span class=" text-red">{{ $message }}</span> @enderror
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 row">
+        <div class="col-lg-12 col-md-12 col-sm-12 ">
             <div class="card card-outline card-primary">
                 <div class="card-header ">
-                    <h5 class="card-title">FORM   <b>IDENTITAS CONTOH</b></h5>
+                    <h5 class="card-title"><i class="fa fa-flask" aria-hidden="true"></i> FORM <b>IDENTITAS CONTOH</b></h5>
                 </div>
                 <div class="card-body row">
+                <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
+                    <label class="control-label col-sm-3"> Jenis Pengujian Sampel <b class='text-red'>*</b></label>
+                    <div class="col-sm-7">
+                        <select wire:model='jenisPengujian' class=" form-control @error('jenisPengujian') is-invalid @enderror">
+                            <option value="">-- Pilih Salah Satu --</option>
+                            @foreach ($analisa as $data )
+                            <option value="{{ $data->id }}">{{ $data->jenis_pengujian}}</option>
+                            @endforeach
+                        <select>
+                        @error('jenisPengujian') <span class=" error is-invalid text-red"> {{ $message }} </span> @enderror
+                    </div>
+                </div>
                 <div class="form-group col-lg-6 col-md-12 col-sm-12 row">
                     <label class="control-label  col-sm-3"> Jumlah Contoh <b class='text-red'>*</b></label>
                     <div class="col-sm-7">
@@ -140,44 +140,50 @@
         <div class="col-lg-12 col-md-12 col-sm-12 ">
             <div class="card card-outline card-primary">
                 <div class="card-header ">
-                    <h5 class="card-title">FORM <b>ITEM PENGUJIAN</b></h5>
+                    <h5 class="card-title"><i class="fa fa-flask" aria-hidden="true"></i> FORM <b>ITEM PENGUJIAN</b></h5>
                 </div>
                 <div class="card-body">
-                <table class="table tabel-sm table-striped table-hover table-bordered">
-                    <thead>
-                        <tr>
-                            <th width="60">No.</th>
-                            <th width="200" class="text-center">Kode Contoh / Sampel (Customer)</th>
-                            <th width="200" class="text-center">Kode Lab</th>
-                            <th width="200" class="text-center">Paremeter Uji</th>
-                            <th width="200" class="text-center">Keterangan</th>
-                            <th width="200" class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ( $sampel as $no=>$data )
-                        <tr>
-                            <td>{{ $no+1 }}.</td>
-                            <td><input wire:model='kodeSampel.{{ $no }}' type="text"  class="form-control form-control-sm rounded-0"></td>
-                            <td><input type="text" class="form-control"></td>
-                            <td><input type="text" class="form-control"></td>
-                            <td><input type="text" class="form-control"></td>
-                            @if($no===0)
-                                <td class="text-center"><a class="btn btn-primary" wire:click="addSampel('{{ $no }}')">+</a></td>
-                            @else
-                                <td class="text-center"><a class="btn btn-danger" wire:click="removeSampel({{ $no }})"><i class="fa fa-trash"></i></a></td>
-                            @endif
+                    <table class="table tabel-sm table-striped table-hover table-bordered">
+                        <thead>
+                            <tr class="text-uppercase">
+                                <th width="60">No.</th>
+                                <th width="200" class="text-center">Kode Contoh / Sampel (Customer) <b class='text-red'>*</b></th>
+                                <th width="200" class="text-center">Kode Lab <b class='text-red'>*</b></th>
+                                <th width="200" class="text-center">Paremeter Uji <b class='text-red'>*</b></th>
+                                <th width="200" class="text-center">Keterangan</th>
+                                <th width="200" class="text-center">Biaya</th>
+                                <th width="200" class="text-center">Item Pengujian</th>
+                                <th width="200" class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ( $sampel as $no=>$data )
+                            <tr>
+                                <td>{{ $no+1 }}.</td>
+                                <td><input placeholder="Kode Sampel" wire:model='kodeSampel.{{ $no }}' type="text"  class="form-control form-control-sm rounded-0"></td>
+                                <td><input placeholder="Kode Lab" type="text" class="form-control form-control-sm rounded-0"></td>
+                                <td><input type="text" class="form-control form-control-sm rounded-0"></td>
+                                <td><input placeholder="Keterangan" type="text" class="form-control form-control-sm rounded-0"></td>
+                                <td></td>
+                                <td></td>
+                                @if($no===0)
+                                    <td class="text-center"><a class="btn btn-primary" wire:click="addSampel('{{ $no }}')">+</a></td>
+                                @else
+                                    <td class="text-center"><a class="btn btn-danger" wire:click="removeSampel({{ $no }})"><i class="fa fa-trash"></i></a></td>
+                                @endif
 
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    <div class="col-md-12 col-lg-12 col-sm-12">
+                        <a href="" class=" btn-md btn btn-danger  float-right" style="margin-right:15px;"><i class="fa fa-times" aria-hidden="true"></i> Batal</a>
+                        <button class=" btn-md btn btn-primary  float-right" style="margin-right:15px;"><i class="fa fa-save" aria-hidden="true"></i> Simpan</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-12 col-lg-12 col-sm-12">
-            <a class=" btn-md btn btn-danger btn=flat rounded-0 float-right" style="margin-right:15px;"><span aria-hidden="true">x</span> Batal</a>
-            <button class=" btn-md btn btn-primary rounded-0 btn=flat float-right" style="margin-right:15px;"><i class="fa fa-save" aria-hidden="true"></i> Simpan</button>
         </div>
     </form>
 </div>
