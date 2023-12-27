202dd00121e7bfe500e1374eaf55722c5a21b38c
ariston <aristonbobi@gmail.com>
2023-12-27 20:15:07 +0800

    update

---
+++ database/migrations/2023_12_27_115431_create_user_pemohons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_pemohons', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->char('no_tlpn',16)->index();
+            $table->foreignId('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_pemohons');
+    }
+};
