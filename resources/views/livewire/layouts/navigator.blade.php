c06a8ef96b61a0fe094589739358f37873501d6c
Ariston-mac <aristonbobi@gmail.com>
2024-01-11 10:21:34 +0800

    update

--- resources/views/livewire/layouts/navigator.blade.php
+++ resources/views/livewire/layouts/navigator.blade.php
@@ -13,11 +13,29 @@
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{ route('permohonanAnalis') }}" class="nav-link">
-              <i class="nav-icon fa fa-envelope" aria-hidden="true"></i>
-              <p>Form Permohonan Analis</p>
+            <a class="nav-link">
+                <i class="nav-icon fas fa-edit" aria-hidden="true"></i>
+                <p>
+                    Pendaftaran Analis
+                    <i class="fas fa-angle-left right"></i>
+                </p>
             </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{ route('permohonanAnalis') }}" class="nav-link">
+                    <i class="nav-icon fa fa-envelope" aria-hidden="true"></i>
+                    <p>Permintaan Analis</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('permohonanAnalis') }}" class="nav-link">
+                      <i class="nav-icon fa fa-envelope" aria-hidden="true"></i>
+                      <p>Data Analis</p>
+                    </a>
+                </li>
+            </ul>
         </li>
+
         <li class="nav-item">
           <a href="#" class="nav-link">
             <i class="nav-icon far fa-plus-square"></i>
