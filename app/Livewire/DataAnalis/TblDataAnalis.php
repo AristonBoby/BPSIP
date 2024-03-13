0a87f4be5bfc33bfb1c32df8c6a06ee1108e6887
Aristonbobi <aristonbobi@gmail.com>
2024-03-13 09:07:47 +0800

    update

--- app/Livewire/DataAnalis/TblDataAnalis.php
+++ app/Livewire/DataAnalis/TblDataAnalis.php
@@ -13,6 +13,7 @@ use App\Models\permintaanAnalisa;
 use App\Models\transaksiAnalisa;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
+use PDO;
 
 class TblDataAnalis extends Component
 {
@@ -85,8 +86,11 @@ class TblDataAnalis extends Component
     public function delete($id)
     {
 
-        $delete = transaksiAnalisa::where('item_analisa_id',$id)->get();
-        dd($delete);
+        $delete = itemAnalisa::where('permintaan_analisas_id',$id)->delete();
+        if($delete)
+        {
+            $data = permintaanAnalisa::find($id)->delete();
+        }
         $this->render();
     }
 }
