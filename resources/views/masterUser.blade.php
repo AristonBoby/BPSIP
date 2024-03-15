b82f1b007ab7557079ba9e6f6965b7c2cd97a9dd
Ariston Boby <aristonbobi@gmail.com>
2024-03-15 10:42:59 +0800

    update

--- resources/views/masterUser.blade.php
+++ resources/views/masterUser.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('header')
-Dashboard
+Data User
 @endsection
 
 @section('navigator')
@@ -10,13 +10,44 @@ Dashboard
 
 @section('content')
     <div class="col-md-12">
-        <div class="card card-outline card-info col-lg-12">
-            <div class="card-header border-0">
-
+       <div class="card card-primary card-outline col-md-4">
+            <div class="card-header">
+                <h5 class="col-label">Form Pendaftaran User</h5> 
             </div>
             <div class="card-body">
-
+                <form wire:submit="formUser">
+                    <div class="form-group row">
+                        <label class="control-label col-lg-3">Nama</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" wire:model="nama">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="control-label col-lg-3">Email</label>
+                        <div class="col-md-9">
+                            <input type="email" class="form-control" wire:model="email">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="control-label col-lg-3">No.Hp</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" wire:model="noHp">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="control-label col-lg-3">Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" wire:model="password">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="control-label col-lg-3">Re Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" wire:model="re_password">
+                        </div>
+                    </div>
+                </form>
             </div>
-        </div>
+       </div>
     </div>
 @endsection
