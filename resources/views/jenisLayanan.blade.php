70a25f46d501189be0706e142c865faab957589d
Ariston Boby <aristonbobi@gmail.com>
2024-04-05 10:10:44 +0800

    update

--- resources/views/jenisLayanan.blade.php
+++ resources/views/jenisLayanan.blade.php
@@ -5,27 +5,5 @@
 @endsection
 
 @section('slide')
-    <div class="card">
-        <div class="card-header">
-            <h5>JENIS LAYANAN</h5>
-        </div>
-        <div class="card-body p-0">
-            <ul class="nav nav-pills flex-column">
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <h5>Inbox</h5>
-                        <span class="text-xs" style="margin-top:10px;"><i>Sabtu, 07 November 2023</i></span>
-                        <hr>
-                        <div>
-                            <span>jssjkjskjskjkjk</span>
-                        <div>
-                    </a>
-                </li>
-                <a href="#" class="nav-link">
-                        <i class="fas fa-inbox"></i> Inbox
-                        <span class="badge bg-primary float-right">12</span>
-                </a>
-            </ul>
-        </div>
-    </div>
+
 @endsection
