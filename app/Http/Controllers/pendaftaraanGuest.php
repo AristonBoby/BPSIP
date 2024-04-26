e311ebf6ff0afbe5064435b6b32d1782316b8bc7
Ariston Boby <aristonbobi@gmail.com>
2024-04-26 11:12:24 +0800

    update

--- app/Http/Controllers/pendaftaraanGuest.php
+++ app/Http/Controllers/pendaftaraanGuest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\provinsi;
+use App\Models\kelurahan;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,9 +11,12 @@ class pendaftaraanGuest extends Controller
 {
     public function index()
     {
-        $prov = provinsi::all();
-
+        $prov = kelurahan::paginate(10);
         return view('pendaftaran',compact('prov'));
+    }
+    public function kota()
+    {
+
     }
 
     public function store(Request $request)
--- resources/views/pendaftaran.blade.php
+++ resources/views/pendaftaran.blade.php
@@ -5,14 +5,8 @@
 @endsection
 
 @section('slide')
-<div class="form-group">
-    <label class="label-control">*Provinsi</label>
-    <select class="form-control select2-hidden-accessible" id="provinsi" style="width: 100%;:100px;"  tabindex="-1" aria-hidden="true">
-        <option value="" selected></option>
-        <option value="AL">Alabama</option>
-        <option value="WY">Wyoming</option>
-    </select>
-</div>
+
+
 <div>
     <div class="card card-default ">
         <div class="card-body"><h5 >Pendaftaran</h5></div>
@@ -26,32 +20,41 @@
             <form action="{{route('guest.daftar')}}" method="POST" >
                 @csrf
                 <div class="form-group">
-                    <label class="label-control">Nama</label>
+                    <label class="label-control"><i class="text-danger">*</i>Nama</label>
                     <input type="text" class="form-control" name="nama" placeholder="Masukan nama anda">
                 </div>
                 <div class="form-group">
-                    <label class="label-control">Email</label>
+                    <label class="label-control"><i class="text-danger">*</i>Email</label>
                     <input type="email" class="form-control" name="email" placeholder="Masukan email anda">
                 </div>
                 <div class="form-group">
-                    <label class="label-control">No HP</label>
+                    <label class="label-control"><i class="text-danger">*</i>No HP</label>
                     <input type="text" class="form-control" name="no_hp" placeholder="Masukan HP anda">
                 </div>
 
                 <div class="form-group">
-                    <label class="label-control">Alamat</label>
+                    <label class="label-control">*Provinsi</label>
+                    <select class="form-control select2-hidden-accessible rounded-0" id="provinsi" style="width:100%;  font-size:1.2em;"  tabindex="-1" aria-hidden="true">
+                        <option value="" selected></option>
+                            @foreach($prov as $data)
+                                <option value="{{$data->id}}" >Provinsi {{$data->kecamatan->kota->provinsi->namaProvinsi}} || Kab/Kota {{$data->kecamatan->kota->namaKota}} || Kecamatan {{$data->kecamatan->namaKecamatan}} || Desa/ Keluarahan {{$data->namaKelurahan}}</option>
+                            @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label class="label-control"><i class="text-danger">*</i> Alamat</label>
                     <input type="text" class="form-control" placeholder="Masukan nama anda">
                 </div>
                 <div class="form-group">
-                    <label class="label-control">Password</label>
+                    <label class="label-control"><i class="text-danger">*</i> Password</label>
                     <input type="password" class="form-control" name="password" placeholder="Password">
                 </div>
                 <div class="form-group">
-                    <label class="label-control">Ulangi Password</label>
+                    <label class="label-control"> <i class="text-danger">*</i> Ulangi Password</label>
                     <input type="password" class="form-control" name="rePassword" placeholder="Ulangi Password">
                 </div>
                 <div class="form-group mt-5">
-                    <button type="submit" class="form-control btn-primary"> Daftar </button>
+                    <button type="submit" id="daftar" class="form-control btn-primary"> Daftar </button>
                 </div>
 
             </form>
@@ -59,16 +62,22 @@
     </div>
 
     <script type="text/javascript">
+        $('#daftar').click(function(e){
+            alert('ddd');
+        });
+
+
         $(document).ready(function(){
             $('#provinsi').select2({
-                placeholder: "Select a state",
+                theme: "classic",
+                minimumInputLength:0,
+                placeholder: "--- Pilih Kelurahan ---",
                 maximumSelectionLength: 2,
                 allowClear: true,
 
             });
         });
     </script>
-
 </div>
 @endsection
 
